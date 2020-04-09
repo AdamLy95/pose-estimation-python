e06681b7b2344d22c17c2f944f78e2fcbea209cc
Adam Ly <adam.ly@precisebiometrics.com>
2020-04-09 12:05:55 +0200

    fixed bug in pad

--- pose_estimator.py
+++ pose_estimator.py
@@ -17,22 +17,14 @@ class PoseEstimator():
     ).astype('float32'))
 
     def preprocess_img(self,img):
-        # fit the image into a 257x257 square
-        shape = np.r_[img.shape]
-        pad = (shape.max() - shape[:2]).astype('uint32') // 2
-        img_pad = np.pad(
-            img,
-            ((pad[0],pad[0]), (pad[1],pad[1]), (0,0)),
-            mode='constant')
-        img_small = cv2.resize(img_pad, (257, 257))
+        img_small = cv2.resize(img, (257, 257))
         img_small = np.ascontiguousarray(img_small)
         img_norm = self._im_normalize(img_small)
-        return img_pad, img_norm, pad
-
+        return img_norm
 
     def __call__(self,image):
         im_h,im_w = image.shape[:2]
-        img_pad, img_norm, pad  = self.preprocess_img(image)
+        img_norm = self.preprocess_img(image)
         img_norm = np.expand_dims(img_norm, axis=0)
         self.interpreter.set_tensor(self.in_idx[0]['index'], img_norm)
         self.interpreter.invoke()
@@ -46,7 +38,7 @@ class PoseEstimator():
         
         offsetVector = np.array([ [offsets[0,y,x,i],offsets[0,y,x,i+n_keypoints]] for i,(y,x) in enumerate(maximum_likelihood_keypoints.astype(int))])
         
-        coords = maximum_likelihood_keypoints / (np.array([height,width])-1) * im_h + offsetVector*im_h/257
+        coords =  (maximum_likelihood_keypoints / (np.array([height,width])-1) + offsetVector/257)* np.array([im_h,im_w])        
         confidence_scores = [expit(heatmaps[0,y,x,i])  for i,(y,x) in enumerate(maximum_likelihood_keypoints.astype(int))]
         
         return coords,confidence_scores
